641e4cb45c513a64f99a6902e0e1f8bf90f23ebb
Kasra Rahjerdi <krahjerdi@stackoverflow.com>
2016-12-31 19:25:33 -0800

    Not gonna lie, always wanted to be a favicon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 
 from flask_misaka import Misaka as FlaskMisaka
 from utils import MaterializeRenderer
@@ -11,6 +11,11 @@ FlaskMisaka(app, MaterializeRenderer())
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+        'favicon-96x96.png', mimetype='image/vnd.microsoft.icon')
 
 app.secret_key = 'SECRETS_SECRETS_ARE_NO_FUN'
 
